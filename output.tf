d9c7a631039c8137801771c0937adbef565dbbc5
alansnow <mail@alansnow.dev>
2023-06-08 15:33:14 -0400

    first commit

---
+++ output.tf
@@ -0,0 +1,13 @@
+###################################################
+# Define Outputs                                  #
+# Output access from outside this module example: #
+# vpc_cidr = "${module.basic-networking.vpc_id}"  #
+###################################################
+
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+
+output "aws_subnet_public_az1_id" {
+  value = "${aws_subnet.public_subnet_az1.id}"
+}
---
+++ variables.tf
@@ -0,0 +1,33 @@
+###################################################
+# Define Variables                                #
+###################################################
+
+variable "region" {
+    description = "AWS Deployment region"
+}
+
+variable "environment" {
+    description = "Environment for networking setup"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for VPC"
+}
+
+variable "public_subnets_cidr" {
+    description = "CIDR for Public Subnets"
+}
+
+variable "private_subnets_cidr" {
+    description = "CIDR for Private Subnets"
+    type = list
+}
+
+variable "availability_zones" {
+    description = "Availability Zones"
+    type = list
+}
+
+variable "deploy_private_subnets" {
+    description = "Specify if private subnets with nat gateway are to be deployed"
+}
